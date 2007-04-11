acc1b1366d3d54aa68db5d591deb249278b15202
Michael Lawrence <lawremi@iastate.edu>
2007-04-11 00:00:00 +0000

    version 1.2-1

--- R/zzz.R
+++ R/zzz.R
@@ -2,9 +2,9 @@
 {
   if(.Platform$OS.type == "windows"){
      temp <- Sys.getenv("PATH")
-     Sys.putenv("PATH" = paste(normalizePath(file.path(libname, pkgname, "libs")), 
+     Sys.putenv(PATH = paste(normalizePath(file.path(libname, pkgname, "libs")), 
                                file.path(Sys.getenv("R_HOME"), "modules", fsep="\\"), temp, sep=";"))
-     on.exit(Sys.putenv(temp))
+     on.exit(Sys.putenv(PATH = temp))
   }
   library.dynam("rsbml", pkgname, libname)
 }
